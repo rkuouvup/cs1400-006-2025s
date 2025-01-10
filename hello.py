6dd41fd078288763426cfc44bb86db5d4df3fdf8
rkuouvup <rkuouvup@gmail.com>
2025-01-10 14:31:48 -0700

    Jan 10 updates.

---
+++ hello.py
@@ -0,0 +1,4 @@
+mile_speed = input("What is the speed in mile/hr: ")
+int_mile_speed = float(mile_speed)
+metric_speed = int_mile_speed * 1.6
+print(metric_speed)
---
+++ hello_name.py
@@ -0,0 +1,2 @@
+my_name = input("Please enter your name: ")
+print("Hello, " + my_name)
