066c8c5fe019b72ad93b98188952d6b0358dfaf1
rkuouvup <rkuouvup@gmail.com>
2025-01-17 14:49:31 -0700

    Jan 17 in-class exercise

---
+++ speed_eval.py
@@ -0,0 +1,17 @@
+"""
+If the car’s speed is greater than 45 miles per hour, the driver gets the ticket
+"""
+"""
+if car_speed is greater than 45
+then print ticket
+"""
+"""
+if boolean_expression:
+    statements
+"""
+car_speed = float(input("Enter the speed: "))
+if car_speed > 45:
+    print("You get a ticket")
+    print("Please go to the court")
+else:
+    print("Good driver")
