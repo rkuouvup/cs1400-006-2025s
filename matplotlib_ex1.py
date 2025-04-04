d07a9658fd9843a05f7fcc76f8d1336fec707d2d
rkuouvup <rkuouvup@gmail.com>
2025-04-04 17:01:05 -0600

    Adding instructor of selecting proper python interpreter

--- matplotlib_ex1.py
+++ matplotlib_ex1.py
@@ -1,1 +1,11 @@
+"""
+Select the correct Python Intepreter on VSCode:
+1. Open the Command Palette by pressing
+    Windows/Linux: Ctrl+Shift+P
+    Mac: Cmd+Shift+P
+2. Search Python Interpreter
+3. Choose "Python: Select Interpreter"
+4. Choose "Python 3.xx.x ('base') your_python_path  Conda
+"""
+
 import matplotlib.pyplot as plt
